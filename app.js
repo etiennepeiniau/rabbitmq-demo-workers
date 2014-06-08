6f48d822d6e52d9739ff95e9f78c4bc577c21079
lepnio <lepnio@gmail.com>
2014-06-08 20:25:21 +0200

    add amqp + user name + user data

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 'use strict';
 
+  // natives
 var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
   , fs = require('fs')
   , readline = require('readline')
+  // dependencies
+  , io = require('socket.io').listen(app)
+  , amqp = require('amqp')
+  //  customs
   , ActionHandler = require('./lib/action-handler')
   , Action = require('./lib/action');
 
-// read line
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-// app server
-app.listen(8080);
-
+// start app server
 function handler(req, res) {
   fs.readFile(__dirname + '/index.html',
     function (err, data) {
@@ -28,25 +25,51 @@ function handler(req, res) {
       res.end(data);
     });
 }
+app.listen(8080);
 
-// socket.io
-var count = 0;
-io.sockets.on('connection', function (socket) {
-  // create the action handler
-  var actionHandler = new ActionHandler(socket);
-  // create an user and increase count
-  var user = 'user_' + count++;
-  console.log('New user: %s', user);
-  // handle disconnect
-  socket.on('disconnect', function () {
-    console.log('User %s disconnected', user);
-  });
-  // handle readline
-  rl.on('line', function (line) {
-    if (line === 'clear') {
-      actionHandler.handle(new Action('clear', ''));
-    } else {
-      actionHandler.handle(new Action('color', line));
-    }
+// read line TODO Remove
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// create amqp connection and wait for it
+var connection = amqp.createConnection({url: "amqp://guest:guest@localhost:5672"});
+connection.on('ready', function () {
+  // socket.io
+  var count = 0;
+  io.sockets.on('connection', function (socket) {
+    // create the action handler
+    var actionHandler = new ActionHandler(socket);
+    // create an user and increase count
+    var user = 'user_' + count++;
+    console.log('New user: %s', user);
+    socket.emit('user.name', { name: user });
+    // receive user data
+    socket.on('user.data', function( data ) {
+      console.log('User data received %s', data);
+      // creating its queues
+      connection.queue( user + '.queue', { durable: false, autoDelete : true, exclusive : true },  function (queue) {
+        console.log("Queue ready for %s", user);
+        // bind the queue to the tree exchange
+        queue.bind('rabbitmq.demo.direct', queue.name);
+        queue.bind('rabbitmq.demo.fanout', '');
+        queue.bind('rabbitmq.demo.topic', 'TODO'); // TODO Get the user info
+        // handle io disconnect
+        socket.on('disconnect', function () {
+          console.log('User %s disconnected', user);
+          queue.destroy();
+        });
+      });
+    })
+    // handle readline TODO Remove
+    rl.on('line', function (line) {
+      if (line === 'clear') {
+        actionHandler.handle(new Action('clear', ''));
+      } else {
+        actionHandler.handle(new Action('color', line));
+      }
+    });
   });
 });
+
