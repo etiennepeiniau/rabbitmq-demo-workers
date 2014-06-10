2adaac5b563f8c9a617844296e748874d4856de7
lepnio <lepnio@gmail.com>
2014-06-11 00:32:54 +0200

    create queue lib

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var app = require('http').createServer(handler)
 // dependencies
   , io = require('socket.io').listen(app)
   , amqp = require('amqp')
+  , slug = require('slug')
 //  customs
-  , ActionHandler = require('./lib/action-handler')
-  , Action = require('./lib/action');
+  , queueManager = require('./lib/queue.js')
+  , ActionHandler = require('./action-handler')
+  , Action = require('./action');
 
 
 // start app server
@@ -28,46 +30,30 @@ app.listen(8080);
 
 // create amqp connection and wait for socket
 var connection = amqp.createConnection({url: "amqp://guest:guest@localhost:5672"});
+var queueFactory = queueManager.factory(connection);
 connection.on('ready', function () {
   // socket.io
   var count = 0;
   io.sockets.on('connection', function (socket) {
     // create an user and increase count
-    var user = 'user_' + count++;
-    socket.emit('user.name', { name: user });
+    var userName = 'user_' + count++;
+    socket.emit('user.name', { name: userName });
     // receive user data
     socket.on('user.data', function (userData) {
-      // creating its queues
-      handleUserQueue(user + '.queue', userData, new ActionHandler(socket), socket);
+      // creating user queue and action handler
+      var actionHandler = new ActionHandler(socket);
+      var userBindingKey = slug(userData.vendor) + '.' + slug (userData.platform);
+      var queue = queueFactory.createQueue(userName, userBindingKey, function (message) {
+        if (message === 'clear') {
+          actionHandler.handle(new Action('clear', ''));
+        } else {
+          actionHandler.handle(new Action('color', message));
+        }
+      });
+      // handle io disconnect
+      socket.on('disconnect', function () {
+        queue.destroy();
+      });
     });
   });
 });
-
-// handle new user queues
-function handleUserQueue(queueName, userData, actionHandler, socket) {
-  connection.queue(queueName, { durable: false, autoDelete: true, exclusive: true, closeChannelOnUnsubscribe: true }, function (queue) {
-    console.log('Queue %s created', queueName);
-    // bind the queue to the tree exchange
-    queue.bind('rabbitmq.demo.direct', queueName);
-    queue.bind('rabbitmq.demo.fanout', '');
-    queue.bind('rabbitmq.demo.topic', 'TODO'); // TODO Get the user info + slug
-    // subscribe to the queues
-    var consumerTag;
-    queue.subscribe(function (message) {
-      if (message === 'clear') {
-        actionHandler.handle(new Action('clear', ''));
-      } else {
-        actionHandler.handle(new Action('color', message));
-      }
-    }).addCallback(function (ok) {
-        consumerTag = ok.consumerTag;
-      });
-    // handle io disconnect
-    socket.on('disconnect', function () {
-      console.log('Destroy queue %s', queueName);
-      userQueue.unsubscribe(consumerTag); // should be called to close the channel
-      userQueue.destroy();
-    });
-  });
-}
-
